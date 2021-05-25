ade965756f1cf29716a8d953fb8dd8545c214ed5
Sudharsan Kakaraparthy <gupta.ksk@gmail.com>
2021-05-25 09:27:05 -0400

    Remove Duplicate From Array

--- src/main/java/com/sudharsan/leetcode/easy/RemoveDuplicateFromArrayInplace.java
+++ src/main/java/com/sudharsan/leetcode/easy/RemoveDuplicateFromArrayInplace.java
@@ -1,4 +1,45 @@
 package com.sudharsan.leetcode.easy;
 
+import java.util.Arrays;
+
 public class RemoveDuplicateFromArrayInplace {
+
+
+//    [1,1,2]
+//            [0,0,1,1,1,2,2,3,3,4]
+    public static void main(String[] args) {
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+
+        System.out.println(removeDuplicates(nums));
+        Arrays.stream(nums).forEach(i -> System.out.print(i + " "));
+    }
+    public static int removeDuplicates(int[] nums) {
+
+
+        int temp = 0, pointer = 1, length = nums.length;
+
+        while (pointer < length){
+            if ( nums[temp] == nums[pointer]){
+                while ((++pointer)< length) {
+                    if (nums[temp] != nums[pointer]) {
+                        copyAndMoveTemp(nums, ++temp, pointer);
+                    }
+                }
+            }else {
+                temp++;
+                pointer++;
+            }
+        }
+
+        return temp + 1;
+    }
+
+    private static void copyAndMoveTemp(int[] nums, int temp, int pointer) {
+
+        do{
+            nums[temp] = nums[pointer];
+        }while ( pointer < nums.length && nums[temp++] != nums[pointer++] );
+    }
+
+
 }
